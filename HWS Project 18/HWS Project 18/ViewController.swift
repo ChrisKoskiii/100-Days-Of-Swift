91c8ad9ffbb2557ef7d587d8ad7cbab7c29439c2
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2021-10-12 18:12:02 -0400

    Add files via upload

---
+++ HWS Project 18/HWS Project 18/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HWS Project 18
+//
+//  Created by Christopher Koski on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    for i in 1...100 {
+      print("number: \(i)")
+    }
+  }
+
+
+}
+
+// 1) Practiced debugging using the print() method
+// 2) Practiced degbugging using assert()
+// 3) Practiced debugging using breakpoints
+// 4) Viewed the 3D model to debug when stacking or hidden objects as an issue
