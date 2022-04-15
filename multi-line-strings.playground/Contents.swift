c62b263f367d5d55cf7ddb7dc9540569049cbd67
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-15 10:43:40 -0400

    Add multi-line-string playground

---
+++ multi-line-strings.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//""" must be on their own lines
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+//add a \ will let you break up your code only to make it more readable, but wont actually include the line breaks in the product
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Multi line text is used for big bodys of text to make them more readable and to fit into certain views
