6def46bce0ac251ad5e5fe692b12f2bc3c93bf51
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-15 10:48:02 -0400

    Add doubles-and-booleans

---
+++ doubles-and-booleans.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+//doubles hold fractional values, whike ints hold whole numbers
+
+var pi = 3.141
+var num = 3
+
+// you cant mix doubles and ints
+// Swift will decide whether a var is an int or double upon creation
+// bools hold true or false
+
+var awesome = true
+awesome = false
+
+
