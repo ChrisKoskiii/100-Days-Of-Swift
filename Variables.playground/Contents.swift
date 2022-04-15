7798cdac760fa0fa6a530ef056e2400474f4e7d9
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-15 10:35:30 -0400

    Add variables playground

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "Goodbye"
+
+//a var can be changed as often as you want.
+
+greeting = "Whats up?"
+
+//read var as "create new varibale"
+//dont include var if you want to edit an existing variable
