3988b82540c033c2ab2d6579ad6c4b43fa995e4f
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-15 10:38:49 -0400

    Add strings-and-integers playground

---
+++ strings-and-integers.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var age = 38
+
+//You can use _ to make Ints more readable
+
+var population = 8_000_000
+
+//It is not possible to change a string to an int
+
+population = 1999
+age = 40
+
+//Swift is type safe, which means swift will make sure we dont make mistakes with our data.
