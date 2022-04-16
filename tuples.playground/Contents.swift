a39f9b0765ac72569f07be53f5719c01e511bfcf
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-16 08:01:16 -0400

    Add tuples

---
+++ tuples.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// You cant add or remove items. They are fixed in value when created
+
+// You cant change types in a tuple
+
+// Can be accessed by number or naming
+
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.first
+
+// They can hold more than one type
+
+var birthday = (month: "September", day: 18)
+birthday.0
+birthday.1
+birthday.month
+birthday.day
+
+// Dictionaries are similar but can grow or change
+
