2b864dc797b69c6b592dd6b6ee444139f9211630
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-15 20:01:35 -0400

    Add Arrays

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+// Swift will crash if you try to acces an element out of index  
+
