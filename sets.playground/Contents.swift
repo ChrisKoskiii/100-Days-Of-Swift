ae8870b55dc8fbd2886a545a2466351529b2fcd6
Christopher Koski <82855685+ChrisKoskiii@users.noreply.github.com>
2022-04-16 07:56:23 -0400

    Add sets

--- sets.playground/Contents.swift
+++ sets.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Items are stored in a random order
+// All items must be unique
+
+let colors = Set(["red", "green", "blue"])
+
+// Because they are unordered, you cant access items via index
+
+let colors2 = Set(["red", "green", "blue", "red", "green"])
+
+// Duplicate items are ignored
+
+let days = [("sunday", "monday", "tuesday", "wednesday", "monday", "sunday")]
+
+
+// Because order doesnt matter, they are optimized for fast retrieval. Arrays will check every item before returning.
+
+// We use sets for times when we want to know "Does this item exist?"
